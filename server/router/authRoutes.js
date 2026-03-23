6513a8145ebf7407ff91cd56599fc4c6f845db52
Le Tien Thanh <letienthanhthptbs@gmail.com>
2026-03-23 15:35:14 +0700

    feat: built authController successfully

--- server/router/authRoutes.js
+++ server/router/authRoutes.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import { getUser, login, logout, register } from '../controllers/authController.js';
+import { isAuthenticated } from '../middlewares/authMiddleware.js';
 
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.get("/me", getUser);
-router.get("/logout", logout);
+router.get("/me", isAuthenticated, getUser);
+router.get("/logout", isAuthenticated, logout);
 
 
 export default router;
