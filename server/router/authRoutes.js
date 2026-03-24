895e68c0f099a06f06ee0f502880832c305d0712
Le Tien Thanh <letienthanhthptbs@gmail.com>
2026-03-24 16:39:27 +0700

    update password successfully

--- server/router/authRoutes.js
+++ server/router/authRoutes.js
@@ -1,18 +1,23 @@
 import express from 'express';
-import { forgotPassword, getUser, login, logout, register, resetPassword, updatePassword } from '../controllers/authController.js';
+import {
+    forgotPassword,
+    getUser,
+    login,
+    logout,
+    register,
+    resetPassword,
+    updatePassword,
+} from '../controllers/authController.js';
 import { isAuthenticated } from '../middlewares/authMiddleware.js';
-
 
 const router = express.Router();
 
-router.post("/register", register);
-router.post("/login", login);
-router.get("/me", isAuthenticated, getUser);
-router.get("/logout", isAuthenticated, logout);
-router.post("/password/forgot", forgotPassword);
-router.put("/password/reset/:token", resetPassword);
-router.put("/password/update", updatePassword);
-
-
+router.post('/register', register);
+router.post('/login', login);
+router.get('/me', isAuthenticated, getUser);
+router.get('/logout', isAuthenticated, logout);
+router.post('/password/forgot', forgotPassword);
+router.put('/password/reset/:token', resetPassword);
+router.put('/password/update', isAuthenticated, updatePassword);
 
 export default router;
